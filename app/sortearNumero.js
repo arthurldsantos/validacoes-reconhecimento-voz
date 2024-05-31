6a752075a0e710f3021ba6caee8a733b0b8084b9
Arthur Santos <arthurldsantos@gmail.com>
2024-05-31 17:09:51 -0300

    codigos js para sortear numero aleatorio e reconhecimento de voz

---
+++ app/sortearNumero.js
@@ -0,0 +1,14 @@
+const menorValor = 1;
+const maiorValor = 1000;
+const numeroSecreto = gerarNumeroAleatorio();
+const elementoMenorValor = document.getElementById('menor-valor');
+const elementoMaiorValor = document.getElementById('maior-valor');
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor + 1);
+}
+
+console.log('Numero Secreto:', numeroSecreto);
+
+elementoMenorValor.innerHTML = menorValor;
+elementoMaiorValor.innerHTML = maiorValor;
